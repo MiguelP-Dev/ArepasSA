34eb20acf5c19f0f3cd2cec2bf4938c1925fb8d2
Miguel Portillo <miguelportillo2475@gmail.com>
2025-08-01 11:47:55 -0500

    update: client model

--- internal/models/client.go
+++ internal/models/client.go
@@ -1,26 +1,25 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
+	"time"
 )
 
 type Client struct {
 	gorm.Model
-	Name     string `gorm:"not null" validate:"required,min=3"`
-	Email    string `validate:"omitempty,email"`
-	Phone    string `validate:"omitempty,numeric"`
-	Address  string
-	Comments []ClientComment `gorm:"foreignKey:ClientID"`
-	IsActive bool            `gorm:"default:true"`
+	Name      string         `gorm:"not null" validate:"required,min=3"`
+	Email     string         `validate:"omitempty,email"`
+	Phone     string         `validate:"omitempty,numeric"`
+	Address   string
+	Comments  []ClientComment `gorm:"foreignKey:ClientID"`
+	IsActive  bool           `gorm:"default:true"`
 }
 
 type ClientComment struct {
 	gorm.Model
-	ClientID     uint      `gorm:"not null" validate:"required"`
-	Author       string    `gorm:"not null"` // Puede ser "system" o el usuario que hizo el comentario
-	Content      string    `gorm:"not null" validate:"required,min=5"`
-	CommentAt    time.Time `gorm:"not null"`
-	IsPreference bool      `gorm:"default:false"` // Para marcar comentarios sobre preferencias
+	ClientID    uint      `gorm:"not null" validate:"required"`
+	Author      string    `gorm:"not null" validate:"required"` // "system" o username
+	Content     string    `gorm:"not null" validate:"required,min=5,max=500"`
+	CommentType string    `gorm:"not null" validate:"oneof=preference general note"` // tipos de comentario
+	CommentDate time.Time `gorm:"not null"`
 }
